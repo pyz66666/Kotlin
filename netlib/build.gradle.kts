529bfbced820d3e873ef992de4e0b418506ee9ff
pyz <893758346@qq.com>
2025-03-16 00:31:01 +0800

    3.15 提交NetSdk，前几天有事

--- netlib/build.gradle.kts
+++ netlib/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
 }
 
 android {
-    namespace = "com.pyz.myapplication"
+    namespace = "com.pyz.netlib"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.pyz.myapplication"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -33,9 +30,6 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    viewBinding {
-        enable = true
-    }
 }
 
 dependencies {
@@ -43,13 +37,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.legacy.support.v4)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.fragment.ktx)
-    implementation(project(":netlib"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ netlib/src/main/java/com/pyz/netlib/NetSdk.kt
@@ -0,0 +1,7 @@
+package com.pyz.netlib
+
+object NetSdk {
+
+
+
+}
